b150d451aa74f3c3e8bd912fefc2e0b2304dcd0c
Gianpero Bozza <gianpiero.bozza@ibillboard.com>
2021-07-20 09:18:37 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Gianpiero Bozza on 20.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
