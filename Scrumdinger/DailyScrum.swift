b633b048fb12cb56c6e86b5363a44b6962b03fd8
Gianpero Bozza <gianpiero.bozza@ibillboard.com>
2021-07-27 08:58:14 +0200

    update colors

--- Scrumdinger/DailyScrum.swift
+++ Scrumdinger/DailyScrum.swift
@@ -19,9 +19,9 @@ struct DailyScrum: Identifiable {
 extension DailyScrum {
     static var data: [DailyScrum] {
         [
-            DailyScrum(title: "Mobbing", attendees: ["GiBo", "MaKl", "OnKo", "JiLe", "MaBe"], lengthInMinutes: 10, color: Color("Mobbing")),
-            DailyScrum(title: "Daily Standup", attendees: ["GiBo", "MaKl", "OnKo", "JiLe", "MaBe"], lengthInMinutes: 15, color: Color("Daily Standup")),
-            DailyScrum(title: "Retro", attendees: ["GiBo", "MaKl", "OnKo", "JiLe", "MaBe", "NaQu"], lengthInMinutes: 60, color: Color("Retro"))
+            DailyScrum(title: "Mobbing", attendees: ["GiBo", "MaKl", "OnKo", "JiLe", "MaBe"], lengthInMinutes: 10, color: Color.yellow),
+            DailyScrum(title: "Daily Standup", attendees: ["GiBo", "MaKl", "OnKo", "JiLe", "MaBe"], lengthInMinutes: 15, color: Color.orange),
+            DailyScrum(title: "Retro", attendees: ["GiBo", "MaKl", "OnKo", "JiLe", "MaBe", "NaQu"], lengthInMinutes: 60, color: Color.blue)
         ]
     }
 }
