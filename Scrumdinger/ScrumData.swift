13e177ad0039a3fb4616cb0591f26b26f344411a
Gianpero Bozza <gianpiero.bozza@ibillboard.com>
2021-08-05 10:52:56 +0200

    finished section 3 step 5 in https://developer.apple.com/tutorials/app-dev-training/persisting-data#Add-a-Method-to-Load-Data

---
+++ Scrumdinger/ScrumData.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+class ScrumData: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    private static var documentsFolder: URL {
+        do {
+            return try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        } catch {
+            fatalError("Can't find documents directory")
+        }
+    }
+    private static var fileURL: URL {
+        return documentsFolder.appendingPathComponent("scrums.data")
+    }
+    
+    func load() {
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let data = try? Data(contentsOf: Self.fileURL) else {
+                #if DEBUG
+                DispatchQueue.main.async {
+                    self?.scrums = DailyScrum.data
+                }
+                #endif
+                return
+            }
+        }
+    }
+}
